55537b608b40c1849a4b42b2828a887bf4418a32
sub-t <subt@outlook.jp>
2022-08-23 17:39:34 +0900

    [update] Avatarの実装

--- src/components/Elements/Avatar/Avatar.tsx
+++ src/components/Elements/Avatar/Avatar.tsx
@@ -1,5 +1,25 @@
-// TODO
+import clsx from 'clsx';
+import { FallbackImage } from '../FallbackImage';
+import type { WithClassName } from '@/types';
 
-export const Avatar = () => {
-  return <></>;
+const sizes = {
+  sm: 'w-10 h-10',
+  lg: 'w-12 h-12',
+  full: 'w-full',
+};
+
+type Props = WithClassName & {
+  src: string | null | undefined;
+  size?: keyof typeof sizes;
+};
+
+export const Avatar = ({ src, size = 'sm', className }: Props) => {
+  return (
+    <>
+      <FallbackImage
+        src={src}
+        className={clsx('overflow-hidden rounded-full', sizes[size], className)}
+      />
+    </>
+  );
 };
