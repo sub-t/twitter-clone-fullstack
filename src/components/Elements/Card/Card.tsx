d8fe8518ce99400db2e10bcc79a11d4d32249b2c
sub-t <subt@outlook.jp>
2022-08-23 17:40:09 +0900

    [update] Cardの実装

--- src/components/Elements/Card/Card.tsx
+++ src/components/Elements/Card/Card.tsx
@@ -1,5 +1,53 @@
-// TODO
+import React from 'react';
+import clsx from 'clsx';
+import type { WithClassName } from '@/types';
 
-export const Card = () => {
-  return <></>;
+type Props = WithClassName & {
+  thumbnail: React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  content?: React.ReactNode;
+  buttons?: React.ReactNode;
+  thread?: boolean;
+};
+
+export const Card = ({
+  thumbnail,
+  header,
+  icon,
+  content,
+  buttons,
+  className,
+  thread = false,
+}: Props) => {
+  return (
+    <div className={clsx('w-full space-y-3 px-4 py-3 bg-white', className)}>
+      <div className="min-w-0 flex">
+        <div className="flex-shrink-0 flex flex-col items-center gap-1 w-12 mr-3">
+          {thumbnail}
+        </div>
+
+        <div className="min-w-0 space-y-3 w-full">
+          {header && (
+            <div className="min-w-0 flex justify-between">
+              <div className="min-w-0 flex-shrink flex flex-col">{header}</div>
+              <div className="flex items-center ml-3">{icon}</div>
+            </div>
+          )}
+          {thread || (
+            <>
+              {content}
+              {buttons}
+            </>
+          )}
+        </div>
+      </div>
+      {thread && (
+        <>
+          {content}
+          {buttons}
+        </>
+      )}
+    </div>
+  );
 };
