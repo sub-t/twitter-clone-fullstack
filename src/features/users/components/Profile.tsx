d499473f85fd286bac557ad30be4af0989675fda
sub-t <subt@outlook.jp>
2022-08-24 21:33:00 +0900

    [update] Profileの更新

--- src/features/users/components/Profile.tsx
+++ src/features/users/components/Profile.tsx
@@ -2,11 +2,14 @@ import {
   BellIcon,
   CalendarIcon,
   DotsHorizontalIcon,
+  LinkIcon,
+  LocationMarkerIcon,
 } from '@heroicons/react/outline';
-import { Avatar, FallbackImage, IconButton } from '@/components/Elements';
+import { Avatar, FallbackImage, IconButton, Link } from '@/components/Elements';
 import { useAuth } from '@/features/auth';
 import { Follow, Unfollow } from '@/features/follows';
 import { formatDate } from '@/utils/formatDate';
+import { formatNumber } from '@/utils/formatNumber';
 import { EditProfile } from './EditProfile';
 import type { User } from '../types';
 
@@ -16,7 +19,6 @@ type Props = {
 
 export const Profile = ({ user }: Props) => {
   const { user: authUser } = useAuth();
-  const { id, name, screenName, description, createdAt, followed } = user;
 
   return (
     <div className="bg-white">
@@ -34,7 +36,7 @@ export const Profile = ({ user }: Props) => {
               className="box-content pb-[100%] border-4 border-white"
             />
           </div>
-          {authUser?.id === id ? (
+          {authUser?.id === user.id ? (
             <EditProfile />
           ) : (
             <div className="flex items-start gap-3">
@@ -44,10 +46,10 @@ export const Profile = ({ user }: Props) => {
               <IconButton variant="outline">
                 <BellIcon />
               </IconButton>
-              {followed ? (
-                <Unfollow data={{ id, screenName }} />
+              {user.followed ? (
+                <Unfollow data={{ id: user.id, screenName: user.screenName }} />
               ) : (
-                <Follow data={{ id, screenName }} />
+                <Follow data={{ id: user.id, screenName: user.screenName }} />
               )}
             </div>
           )}
@@ -55,14 +57,47 @@ export const Profile = ({ user }: Props) => {
         <div className="flex flex-col gap-4">
           <div>
             <div className="text-slate-800 dark:text-white text-xl font-extrabold break-words">
-              {name}
+              {user.name}
             </div>
-            <div className="text-slate-500">{`@${screenName}`}</div>
+            <div className="text-slate-500">{`@${user.screenName}`}</div>
           </div>
-          {description && <div className="text-slate-700">{description}</div>}
-          <div className="flex items-center gap-1 text-slate-500">
-            <CalendarIcon className="w-5 h-5" />
-            <span>Joined {formatDate(createdAt)}</span>
+          {user.description && (
+            <div className="text-slate-700">{user.description}</div>
+          )}
+          <div className="flex flex-wrap items-center gap-3 text-slate-500">
+            {user.location && (
+              <span className="flex gap-1">
+                <LocationMarkerIcon className="w-5 h-5" />
+                <span>{user.location}</span>
+              </span>
+            )}
+            {user.url && (
+              <span className="flex gap-1">
+                <LinkIcon className="w-5 h-5" />
+                <a href={user.url} className="text-sky-500">
+                  {new URL(user.url).hostname}
+                </a>
+              </span>
+            )}
+            <span className="flex gap-1">
+              <CalendarIcon className="w-5 h-5" />
+              <span>Joined {formatDate(user.createdAt)}</span>
+            </span>
+          </div>
+          <div className="flex gap-3 text-slate-500">
+            {/* TODO */}
+            <Link href={`/${user.screenName}/following`} className="flex">
+              <span className="text-slate-900 font-bold">
+                {formatNumber(user.friendsCount)}
+              </span>
+              <span>Following</span>
+            </Link>
+            <Link href={`/${user.screenName}/followers`} className="flex">
+              <span className=" text-slate-900 font-bold">
+                {formatNumber(user.followersCount)}
+              </span>
+              <span>Followers</span>
+            </Link>
           </div>
         </div>
       </div>
