eb3c5ee392cb6db676e608e25fb009da8fe7271a
sub-t <subt@outlook.jp>
2022-08-24 21:34:57 +0900

    [update]

--- src/features/tweets/components/TweetCard.tsx
+++ src/features/tweets/components/TweetCard.tsx
@@ -33,87 +33,89 @@ export const TweetCard = ({ data, reply = false }: Props) => {
   const { open: openComposeTweet } = useComposeTweet();
 
   return (
-    <Card
-      thumbnail={
-        <>
-          <Link href={`/${user.screenName}`}>
-            <Avatar src={user?.profileImageUrl} size="lg" />
-          </Link>
-          {reply && <div className="w-[2px] h-full -mb-3 bg-slate-300" />}
-        </>
-      }
-      header={
-        <div className="min-w-0 flex gap-1">
-          <span className="font-bold text-slate-900 truncate">
-            <Link href={`/${user.screenName}`}>{user.name}</Link>
-          </span>
-
-          <div className="min-w-0 flex ml-1 text-slate-600">
-            <span className="truncate">
-              <Link href={`/${user.screenName}`}>{`@${user.screenName}`}</Link>
+    <article>
+      <Card
+        thumbnail={
+          <>
+            <Link href={`/${user.screenName}`}>
+              <Avatar src={user?.profileImageUrl} size="lg" />
+            </Link>
+            {reply && <div className="w-[2px] h-full -mb-3 bg-slate-300" />}
+          </>
+        }
+        header={
+          <div className="min-w-0 flex gap-1">
+            <span className="font-bold text-slate-900 truncate">
+              <Link href={`/${user.screenName}`}>{user.name}</Link>
             </span>
 
-            <span className="flex-shrink-0 mx-1">･</span>
-            <time className="flex-shrink-0">{formatDate(data.createdAt)}</time>
+            <div className="min-w-0 flex ml-1 text-slate-600">
+              <span className="truncate">
+                <Link href={`/${user.screenName}`}>{`@${user.screenName}`}</Link>
+              </span>
+
+              <span className="flex-shrink-0 mx-1">･</span>
+              <time className="flex-shrink-0">{formatDate(data.createdAt)}</time>
+            </div>
           </div>
-        </div>
-      }
-      icon={
-        <DropdownMenu
-          trigger={
-            <IconButton className="-m-2 ml-3">
-              <DotsHorizontalIcon />
-            </IconButton>
-          }
-          animationType="slideIn"
-        >
-          <DropdownMenuGroup className="w-[300px]">
-            {user.id === authUser?.id && (
-              <DropdownMenuItem
-                asChild
-                className="flex items-center gap-3 w-full h-[52px] p-4 text-red-500"
+        }
+        icon={
+          <DropdownMenu
+            trigger={
+              <IconButton className="-m-2 ml-3">
+                <DotsHorizontalIcon />
+              </IconButton>
+            }
+            animationType="slideIn"
+          >
+            <DropdownMenuGroup className="w-[300px]">
+              {user.id === authUser?.id && (
+                <DropdownMenuItem
+                  asChild
+                  className="flex items-center gap-3 w-full h-[52px] p-4 text-red-500"
+                >
+                  <button onClick={() => openDeleteTweet({ tweetId: data.id })}>
+                    <TrashIcon className="flex-shrink-0 w-5 h-5" />
+                    Delete
+                    <div className="flex-grow" />
+                  </button>
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuGroup>
+          </DropdownMenu>
+        }
+        content={<div className="break-words text-slate-900">{data.text}</div>}
+        buttons={
+          <div className="min-w-0 flex justify-between max-w-md">
+            <span className="flex items-center gap-1">
+              <IconButton
+                onClick={() => openComposeTweet({ data })}
+                className="-m-2"
               >
-                <button onClick={() => openDeleteTweet({ tweetId: data.id })}>
-                  <TrashIcon className="flex-shrink-0 w-5 h-5" />
-                  Delete
-                  <div className="flex-grow" />
-                </button>
-              </DropdownMenuItem>
-            )}
-          </DropdownMenuGroup>
-        </DropdownMenu>
-      }
-      content={<div className="break-words text-slate-900">{data.text}</div>}
-      buttons={
-        <div className="min-w-0 flex justify-between max-w-md">
-          <span className="flex items-center gap-1">
-            <IconButton
-              onClick={() => openComposeTweet({ data })}
-              className="-m-2"
-            >
-              <ChatAltIcon />
-            </IconButton>
-            {data.replyCount}
-          </span>
-          <span className="flex items-center gap-1">
-            <IconButton variant="success" className="-m-2">
-              <SwitchHorizontalIcon />
-            </IconButton>
-            {/* retweetCount */}
-          </span>
-          <span className="flex items-center gap-1">
-            <IconButton variant="danger" className="-m-2">
-              <HeartIcon />
-            </IconButton>
-            {data.favoriteCount}
-          </span>
-          <span className="flex items-center gap-1">
-            <IconButton className="-m-2">
-              <UploadIcon />
-            </IconButton>
-          </span>
-        </div>
-      }
-    />
+                <ChatAltIcon />
+              </IconButton>
+              {data.replyCount}
+            </span>
+            <span className="flex items-center gap-1">
+              <IconButton variant="success" className="-m-2">
+                <SwitchHorizontalIcon />
+              </IconButton>
+              {/* retweetCount */}
+            </span>
+            <span className="flex items-center gap-1">
+              <IconButton variant="danger" className="-m-2">
+                <HeartIcon />
+              </IconButton>
+              {data.favoriteCount}
+            </span>
+            <span className="flex items-center gap-1">
+              <IconButton className="-m-2">
+                <UploadIcon />
+              </IconButton>
+            </span>
+          </div>
+        }
+      />
+    </article>
   );
 };
