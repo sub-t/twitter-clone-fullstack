baa62a3305cd4ee3473faf6b601170372374c4d1
sub-t <subt@outlook.jp>
2022-08-25 17:58:37 +0900

    [update] スタイルの修正

--- src/components/Elements/Spacer/Spacer.tsx
+++ src/components/Elements/Spacer/Spacer.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export const Spacer = ({ thin = false }: Props) => {
   return (
-    <div className={clsx('w-full h-[1px] bg-slate-200', thin || 'my-1')} />
+    <div className={clsx('w-full h-[1px] bg-slate-100', thin || 'my-1')} />
   );
 };
