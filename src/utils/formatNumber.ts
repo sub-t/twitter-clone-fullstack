5d8945c5c5303ac7354399caeb06ec7d16009acc
sub-t <subt@outlook.jp>
2022-08-25 17:57:26 +0900

    [fix]

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,4 +1,5 @@
 const lookup = [
+  { value: 0, symbol: '' },
   { value: 1, symbol: '' },
   { value: 1e3, symbol: 'k' },
   { value: 1e6, symbol: 'M' },
@@ -9,5 +10,5 @@ export const formatNumber = (num: number) => {
     .slice()
     .filter(({ value }) => value <= num)
     .pop()!;
-  return value ? Math.round(num / value) + symbol : num.toString();
+  return value > 0 ? Math.round(num / value) + symbol : num.toString();
 };
